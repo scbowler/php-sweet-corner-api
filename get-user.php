de204154a8172828d69eafa661512b3d5bd1fa55
Scott Bowler <scott.bowler@learningfuze.com>
2019-06-14 04:03:52 +0000

    Completed get single user and create user

--- get-user.php
+++ get-user.php
@@ -4,24 +4,36 @@ $output = [
     'success' => false
 ];
 
+$id = null;
+
+if(isset($_GET['user-id'])){
+    $id = $_GET['user-id'];
+} else {
+    $output['error'] = 'No user ID received';
+}
+
 require_once('mysql-connect.php');
 
-if(empty($output['error'])){
-    $query = "SELECT `name`, `email` FROM `userssdf`";
+if(empty($output['error']) && $id){
+    $query = "SELECT `id`, `name`, `email`, `created_at`, `updated_at` FROM `users` WHERE `id`=$id";
 
     $result = mysqli_query($conn, $query);
 
     if($result){
         $output['success'] = true;
-        if(mysqli_num_rows($result)){
-            while($row = mysqli_fetch_assoc($result)){
-                $output['users'][] = $row;
-            }
+        $output['user'] = null;
+
+        $num_rows = mysqli_num_rows($result);
+
+        if($num_rows === 1){
+            $output['user'] = mysqli_fetch_assoc($result);
+        } elseif($num_rows > 1) {
+            $output['message'] = 'Too many users found';
         } else {
-            $output['users'] = [];
+            $output['message'] = "No user with ID of $id found";
         }
     } else {
-        $output['error'] = 'Error getting data';
+        $output['error'] = 'Database error';
     }
 }
 
