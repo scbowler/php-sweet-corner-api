25a96e56613432236b85f2389a22a541fa9a6f3c
Scott Bowler <scott.bowler@learningfuze.com>
2019-06-20 21:08:16 -0700

    Added error handling and started oop mysqli conversion

--- create-user.php
+++ create-user.php
@@ -1,4 +1,6 @@
 <?php
+require_once('setup.php');
+
 $output = [
     'success' => false
 ];
@@ -9,41 +11,43 @@ $password = null;
 
 if(isset($_POST['name'])){
     $name = $_POST['name'];
+} else {
+    $output['errors'][] = 'Missing Name';
 }
 
 if(isset($_POST['email'])){
     $email = $_POST['email'];
+} else {
+    $output['errors'][] = 'Missing Email';
 }
 
 if(isset($_POST['password'])){
     $password = hash('sha256', $_POST['password']);
+} else {
+    $output['errors'][] = 'Missing Password';
 }
 
-if(!$name || !$email || !$password){
-    $output['error'] = 'Missing user data';
+if(!empty($output['errors'])){
+    throw new ApiException($output, 422);
 }
 
-require_once('mysql-connect.php');
+$query = "INSERT INTO `users` (`name`, `email`, `password`, `created_at`, `updated_at`)
+    VALUES ('$name', '$email', '$password', CURRENT_TIME, CURRENT_TIME)";
 
-if(empty($output['error'])){
-    $query = "INSERT INTO `users` (`name`, `email`, `password`, `created_at`, `updated_at`)
-        VALUES ('$name', '$email', '$password', CURRENT_TIME, CURRENT_TIME)";
+$result = $conn->query($query);
 
-    $result = mysqli_query($conn, $query);
+if($result){
+    if($conn->affected_rows){
+        $insertedId = $conn->insert_id;
 
-    if($result){
-        if(mysqli_affected_rows($conn)){
-            $insertedId = mysqli_insert_id($conn);
-
-            $output['success'] = true;
-            $output['user-id'] = $insertedId;
-            $output['message'] = 'User successfully created';
-        } else {
-            $output['error'] = 'User not created';
-        }
+        $output['success'] = true;
+        $output['user-id'] = $insertedId;
+        $output['message'] = 'User successfully created';
     } else {
-        $output['error'] = 'Database error';
+        throw new ApiException($output, 406, 'User not created');
     }
+} else {
+    throw new ApiException($output, 500, 'Database error');
 }
 
 print json_encode($output);
---
+++ error-handle.php
@@ -0,0 +1,29 @@
+<?php
+
+class ApiException extends Exception {
+    private $output;
+
+    public function __construct($output = [], $code = 500, $message = 'internal server error', Exception $previous = null ){
+        $this->output = $output;
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function sendError(){
+        if(empty($this->output['error']) && empty($this->output['errors'])){
+            $this->output['error'] = $this->message;
+        }
+
+        http_response_code($this->code);
+
+        print json_encode($this->output);
+        
+        die();
+    }
+}
+
+function defaultExceptionHandler($ex){
+    $ex->sendError();
+}
+
+set_exception_handler('defaultExceptionHandler');
